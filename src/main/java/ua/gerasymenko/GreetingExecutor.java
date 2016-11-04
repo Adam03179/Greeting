5043f96f6c3431c24c721d824451cc5b8258e88d
Adam03179 <Adam1983@mail.ru>
2016-11-04 15:15:12 +0200

    added verification of Bundle

--- src/main/java/ua/gerasymenko/GreetingExecutor.java
+++ src/main/java/ua/gerasymenko/GreetingExecutor.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class GreetingExecutor {
@@ -15,24 +16,28 @@ public class GreetingExecutor {
 
 
         Locale locale = Locale.getDefault();
-        ResourceBundle defaultBundle = ResourceBundle.getBundle("greetingBundle",locale);
+
+        ResourceBundle defaultBundle;
+        try {
+            defaultBundle = ResourceBundle.getBundle("greetingBundle", locale);
+        } catch (MissingResourceException e) {
+            defaultBundle = ResourceBundle.getBundle("greetingBundle_en_US");
+        }
+
         Date currentDate = new Date();
 
 
         try {
 
             GreetingGenerator greetingGenerator = new GreetingGenerator();
-            System.out.println(greetingGenerator.greetingsChoice(defaultBundle,currentDate));
+            System.out.println(greetingGenerator.greetingsChoice(defaultBundle, currentDate));
 
         } catch (ParseException e) {
             LOG.error("date greeting error", e);
         }
 
 
-
     }
-
-
 
 
 }
